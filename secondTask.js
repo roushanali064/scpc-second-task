d06bbe1e8dad72d9a2915c57e0023a78de44d054
roushanali064 <roushanrhaman064@gmail.com>
2023-07-27 16:18:03 +0600

    roman number done

--- secondTask.js
+++ secondTask.js
@@ -94,3 +94,35 @@ function generatePassword(){
     console.log(password)
 }
 generatePassword()
+
+//Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value
+function romanToInt(roman) {
+    const romanNumber = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    };
+  
+    let result = 0;
+  
+    for (let i = 0; i < roman.length; i++) {
+      const currentSymbolValue = romanNumber[roman[i]];
+      const nextSymbolValue = romanNumber[roman[i + 1]];
+  
+      if (nextSymbolValue > currentSymbolValue) {
+        result += nextSymbolValue - currentSymbolValue;
+        i++;
+      } else {
+        result += currentSymbolValue;
+      }
+    }
+  
+    return result;
+  }
+
+  console.log(romanToInt("IV"));
+  
