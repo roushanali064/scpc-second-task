8a453bd5180f2defc3510de912318da807894e46
roushanali064 <roushanrhaman064@gmail.com>
2023-07-27 15:18:08 +0600

    task 4 done

--- secondTask.js
+++ secondTask.js
@@ -46,4 +46,29 @@ function mostFrequentNumber(arr) {
 const inputArray = [3, 5, 2, 5, 3, 3, 1, 4, 5];
 const mostFrequent = mostFrequentNumber(inputArray);
 console.log(mostFrequent);
- 
+
+//Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+
+function TwoNumbersWithTargetSum(arr, target) {
+    let firstSide = 0;
+    let secondSide = arr.length - 1;
+  
+    while (firstSide < secondSide) {
+      const currentSum = arr[firstSide] + arr[secondSide];
+  
+      if (currentSum === target) {
+        return [firstSide, secondSide];
+      } else if (currentSum < target) {
+        firstSide++;
+      } else {
+        secondSide--;
+      }
+    }
+  
+    return null;
+  }
+  const arr = [1, 3, 6, 8, 11, 15];
+  const target = 9;
+  const targetSumResult = TwoNumbersWithTargetSum(arr, target);
+  console.log(targetSumResult);
+  
