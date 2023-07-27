545b60dcea14bda7637d77f81af8ff08ff86fcdb
roushanali064 <roushanrhaman064@gmail.com>
2023-07-27 13:06:35 +0600

    task 1 string reverse done

--- secondTask.js
+++ secondTask.js
@@ -1,1 +1,12 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+function stringRevers(str){
+    let reversStr  = '';
+    for(let i = str.length-1; i>=0; i--){
+        const reversCount = str[i];
+        reversStr += reversCount;
+    }
+    return reversStr;
+}
+const result = stringRevers('roushan')
+console.log(result)
+//Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
