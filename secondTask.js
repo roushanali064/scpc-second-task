db2fddd13d0f72583f596bb948ccc19832d952ae
roushanali064 <roushanrhaman064@gmail.com>
2023-07-27 13:40:49 +0600

    task 2 positive number sum done

--- secondTask.js
+++ secondTask.js
@@ -7,6 +7,16 @@ function stringRevers(str){
     }
     return reversStr;
 }
-const result = stringRevers('roushan')
-console.log(result)
+const result = stringRevers('hello world');
+console.log(result);
 //Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+function calculatePositiveNumber(arr){
+    const positiveNumber = arr.filter(item=>item>0);
+    const numberSum = positiveNumber.reduce((previousValue,currentValue)=>previousValue+currentValue,0);
+    return numberSum;
+}
+
+const numberArray = [2, -5, 10, -3, 7];
+const positiveNumberSumResult =  calculatePositiveNumber(numberArray);
+console.log(positiveNumberSumResult);
